16076fa85824b59f23d5d242b582e8c2e800d9ed
0xethen <222267744+0xethen@users.noreply.github.com>
2026-06-28 12:19:41 -0400

    poppa e's kitchen compile fix

--- src/routes/creators/poppa/+page.svelte
+++ src/routes/creators/poppa/+page.svelte
@@ -1,3 +1,18 @@
 <script lang="ts">
-  window.location.replace("https://www.youtube.com/@poppaeskitchen");
+  import { onMount } from "svelte";
+
+  const target = "https://youtube.com/@poppaeskitchen";
+
+  onMount(() => {
+    if (typeof window !== "undefined") {
+      setTimeout(() => {
+        window.location.href = target;
+      }, 5000);
+    }
+  });
 </script>
+
+<a href="https://www.youtube.com/@poppaeskitchen"
+  >Moved to About section on YouTube - "content will be uploaded very soon"</a
+>
+<p class="italic text-sm">Poppa E's Kitchen Rumble page has been deactivated</p>
