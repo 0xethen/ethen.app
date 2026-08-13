e7f3808a2c1c561a416424178c9728f1110c912d
0xethen <222267744+0xethen@users.noreply.github.com>
2026-08-12 21:05:15 -0400

    i do not say bleh bleh bleh

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,9 +5,10 @@ export const Route = createFileRoute("/")({ component: Home });
 function Home() {
   return (
     <div className="p-8 flex flex-col items-center justify-center min-h-dvh">
-      <h1 className="text-3xl font-medium">welcome to ethen.app</h1>
+      <h1 className="text-22xl font-medium">you are on ethen.app</h1>
       <p className="mt-2">
-        Ethen's corner of the web. <Route.Link to="/projects">projects (WIP)</Route.Link>
+        Ethen's corner of the web is a work in progress at the moment.{" "}
+        <Route.Link to="/projects">projects</Route.Link>
       </p>
     </div>
   );
