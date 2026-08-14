e1a6729496aff42a3ac6533d3db7f08b88f301a1
0xethen <222267744+0xethen@users.noreply.github.com>
2026-08-14 14:37:06 -0400

    font mono

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,9 @@ export const Route = createFileRoute("/")({ component: Home });
 function Home() {
   return (
     <div className="p-8 flex flex-col min-h-dvh gap-2 items-center justify-center text-center">
-      <h1 className="text-2xl">you are on <span className="font-medium">ethen.app</span></h1>
+      <h1 className="font-mono text-2xl">
+        you are on <span className="font-medium">ethen.app</span>
+      </h1>
       <p>
         <Route.Link to="/projects">projects</Route.Link>
         <span className="text-muted-fg">{" / "}</span>
@@ -14,12 +16,8 @@ function Home() {
         <Route.Link to="/old">old ethen.app</Route.Link>
       </p>
       <div className="flex flex-col gap-1 items-center text-xs text-muted-fg">
-        <p>
-          Ethen's corner of the web is a work in progress at the moment.
-        </p>
-        <p>
-           Good things are coming soon&trade;
-        </p>
+        <p>Ethen's corner of the web is a work in progress at the moment.</p>
+        <p>Good things are coming soon&trade;</p>
       </div>
     </div>
   );
